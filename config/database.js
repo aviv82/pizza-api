e671ac5aafced1d24dc1e63f59274375cb22bb8f
Aviv <aviv.dror@gmail.com>
2022-04-11 15:10:46 +0200

    upate database.js

--- config/database.js
+++ config/database.js
@@ -1,3 +1,23 @@
+const parse = require("pg-connection-string").parse;
+const config = parse(process.env.DATABASE_URL);
+module.exports = ({ env }) => ({
+  connection: {
+    client: "postgres",
+    connection: {
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+    debug: false,
+  },
+});
+
+/*
 const path = require('path');
 
 module.exports = ({ env }) => ({
@@ -9,3 +29,4 @@ module.exports = ({ env }) => ({
     useNullAsDefault: true,
   },
 });
+*/
